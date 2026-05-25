90092f7a7cb4ad962cbe65ba5942eacbbe2acb17
munamchand <munamchand20@gmail.com>
2026-05-25 14:47:23 +0500

    New file and code

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ print("This is the Sombor Index Calculator")
 print("Hello World")
 print("This is the Sombor Index Calculator")
 print("Hello World")
+import networkx as nx
+
+G = nx.path_graph(5)
+
+print("Vertices:", G.number_of_nodes())
+print("Edges:", G.number_of_edges())
